8e79d81973eb24973538adc187aa3eaf8bc2b834
0rregol <orregol@gmail.com>
2025-11-29 11:15:39 -0300

    251129_03

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/logout', function(req, res, next) {
     req.logout(function(err) {
       if (err) { return next(err); } 
       res.redirect('/');
-    });
-  }   
+    });  
+    }); 
 
 module.exports = router;
